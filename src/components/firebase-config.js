bd69a4b97958ad3af34159daaced24ef34e59713
Zeiyar Lynn <zeiyarlynn@gmail.com>
2022-04-01 11:52:59 +0630

    updated config

--- src/components/firebase-config.js
+++ src/components/firebase-config.js
@@ -3,6 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyAl4NF33hWFlwVEHEpmKZ4w24pwQAdiFGY",
+  authDomain: "trello-clone-91cb2.firebaseapp.com",
+  projectId: "trello-clone-91cb2",
+  storageBucket: "trello-clone-91cb2.appspot.com",
+  messagingSenderId: "364558531880",
+  appId: "1:364558531880:web:b3eed22a5fd9f688b7641d"
 };
 
 const app = initializeApp(firebaseConfig); // init firebase
